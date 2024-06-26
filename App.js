3ebcba7f295695cdb4e766cfe3c6498052399567
jisubin <jyepjhhhh@gmail.com>
2024-06-26 15:59:45 +0900

    commit

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect} from 'react';
+import axios from 'axios';
+
 
 export default function App() {
+
+  const localhost = "192.168.55.35";
+
+  useEffect(() => {
+
+    const test = async () => {
+      try {
+        // 예를 들어, 사용자 ID를 서버로 보내 관심 매장 목록을 요청하는 경우
+        const response = await axios.post(`http://${localhost}:8090/nuvida/test`);
+        console.log(response.data)
+      } catch (error) {
+        console.error(error );
+      }
+    };
+
+    test();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
   );
 }
 
