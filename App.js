3aee29834d5e0ba34fbb7b14a06e07d3eb9543f0
jinew <p5653900415@gmail.com>
2024-07-27 16:01:16 +0900

    연결확인

--- App.js
+++ App.js
@@ -4,15 +4,19 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import signin from './screen/signin';
 import signUp from './screen/signUp';
+import calculate from './screen/calculate';
+import reservationInfo from './screen/reservationInfo';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Signin">
+        <Stack.Navigator initialRouteName="reservationInfo">
           <Stack.Screen name="signin" component={signin} options={{ headerShown: false }}/>
           <Stack.Screen name="signUp" component={signUp} options={{ headerShown: false }}/>
+            <Stack.Screen name="calculate" component={calculate} options={{ headerShown: false }}/>
+            <Stack.Screen name="reservationInfo" component={reservationInfo} options={{ headerShown: false }}/>
         </Stack.Navigator>
         <StatusBar style="auto" />
       </NavigationContainer>
