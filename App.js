7a88c434cad696e8417074db783da845722c6df1
jinew <p5653900415@gmail.com>
2024-08-04 20:56:09 +0900

    연결확인

--- App.js
+++ App.js
@@ -6,17 +6,23 @@ import signin from './screen/signin';
 import signUp from './screen/signUp';
 import calculate from './screen/calculate';
 import reservationInfo from './screen/reservationInfo';
+import memberList from './screen/memberList';
+import tripSchedule from "./screen/tripSchedule";
+import tripCalendar from "./screen/tripCalendar";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="reservationInfo">
+        <Stack.Navigator initialRouteName="tripCalendar">
           <Stack.Screen name="signin" component={signin} options={{ headerShown: false }}/>
           <Stack.Screen name="signUp" component={signUp} options={{ headerShown: false }}/>
             <Stack.Screen name="calculate" component={calculate} options={{ headerShown: false }}/>
             <Stack.Screen name="reservationInfo" component={reservationInfo} options={{ headerShown: false }}/>
+            <Stack.Screen name="memberList" component={memberList} options={{headerShown: false}}/>
+            <Stack.Screen name="tripSchedule" component={tripSchedule} options={{headerShown: false}}/>
+            <Stack.Screen name="tripCalendar" component={tripCalendar} options={{headerShown: false}}/>
         </Stack.Navigator>
         <StatusBar style="auto" />
       </NavigationContainer>
