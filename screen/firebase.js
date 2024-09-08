d4119ebb00480ec58c7180acdf1a5faa5a420778
jisubin <jyepjhhhh@gmail.com>
2024-09-08 16:20:36 +0900

    api키

--- screen/firebase.js
+++ screen/firebase.js
@@ -4,12 +4,7 @@ import { getFirestore } from 'firebase/firestore'; // Firestore 모듈 가져오
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDZuZsmJDYEWp5arnfRc6pSqMd0WAt01kU",
-    authDomain: "high-service-431903-t6.firebaseapp.com",
-    projectId: "high-service-431903-t6",
-    storageBucket: "high-service-431903-t6.appspot.com",
-    messagingSenderId: "797041135189",
-    appId: "1:797041135189:android:bfe45b46755c233195cedc"
+
 };
 
 // Firebase 초기화
